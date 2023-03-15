cde9507fe4c29acd0c6aeb2348b5c083b60d8741
Long Ha-Hoang <github@ha.mozmail.com>
2023-03-15 22:42:19 +0100

    add: imprint and data privacy pages

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,17 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  console.log("Hello World!");
   return (
     <>
       <header className={styles.header}>
-        <h1>This is the website header</h1>
+        <h1>Hi my name is Long</h1>
       </header>
       <main className={styles.main}>
-        <p>Website under construction.</p>
+        <p>This website is under construction.</p>
       </main>
       <footer>
         <Link href={"/Imprint"}>Imprint (Impressum)</Link>
+        <Link href={"/DataPrivacy"}>Data Privacy (Datenschutz)</Link>
       </footer>
     </>
   );
